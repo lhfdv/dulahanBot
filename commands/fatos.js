28cef8feb49503449ae9838ae992247264c38b3e
lhfdv <47636364+lhfdv@users.noreply.github.com>
2020-11-12 21:18:31 -0300

    Update fatos.js
    lol

--- commands/fatos.js
+++ commands/fatos.js
@@ -17,6 +17,8 @@ module.exports = {
 			'Uma delícia de orelha de Viera',
 			'Baseado em fatos Harumisticos',
 			'Eu concordo com',
+			'Uma pergunta, ja terminei a MSQ, upei todos os crafts, upei todas as jobs, destravei todo conteudo, dei clear em todos os extreme, savage, ultimate e unreals, comprei mansão, entrei na comunidade de RP, instalei mods de todos os tipos, fiz uma FC rank máximo com 200 membros, participo e tenho varias linkshells, ja fiz 999kk em todos os meus 9 retainers, coletei todas as armas, gears, mounts, titles,minions, mas ainda não entendi, quando que o jogo fica divertido?',
+			'as vezes penso'
 		]
 		const response = phrases[Math.floor(Math.random() * phrases.length)];
 		return msg.channel.send(response);
