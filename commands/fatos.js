c466ad818e14d9d4a938c9cf737e394c5d77c2b9
lhfdv <47636364+lhfdv@users.noreply.github.com>
2020-12-16 23:44:05 -0300

    lol

--- commands/fatos.js
+++ commands/fatos.js
@@ -20,7 +20,12 @@ module.exports = {
 			'Uma pergunta, ja terminei a MSQ, upei todos os crafts, upei todas as jobs, destravei todo conteudo, dei clear em todos os extreme, savage, ultimate e unreals, comprei mansão, entrei na comunidade de RP, instalei mods de todos os tipos, fiz uma FC rank máximo com 200 membros, participo e tenho varias linkshells, ja fiz 999kk em todos os meus 9 retainers, coletei todas as armas, gears, mounts, titles,minions, mas ainda não entendi, quando que o jogo fica divertido?',
 			'as vezes penso',
 			'kkkkk eles jogam joguinho de tiro',
-			'pera que ta chovendo'
+			'pera que ta chovendo',
+			'Bom? Bom pra quem?',
+			'Aprende com o João Pedro, aprende a dar porrada nos outros',
+			'Verdade Harumi',
+			'KKKKKKKKKKKK ELES JOGAM JOGUINHOS',
+			'Mocha Branco é muito bom'
 		]
 		const response = phrases[Math.floor(Math.random() * phrases.length)];
 		return msg.channel.send(response);
