977bcda8959759d4b3133a6f8a947b16d6589411
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-07 20:56:10 -0300

    Update bonk.js

--- commands/bonk.js
+++ commands/bonk.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'bonk',
 	description: 'bonk',
 	execute(msg) {
-        return msg.channel.send(`${this.getMentionedUsernames(msg)}, you've been kissed by **${msg.author.username}**. :kiss:`, {files: ["https://media1.tenor.com/images/4dee992174206c66cb208bee31174b8d/tenor.gif?itemid=18805247"]});
+        return msg.channel.send({files: ["https://media1.tenor.com/images/4dee992174206c66cb208bee31174b8d/tenor.gif?itemid=18805247"]});
 	},
 };
