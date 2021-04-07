2c64c8fa9a7358e25130079620a34840513dfc68
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-07 20:03:20 -0300

    member reference

--- commands/bonk.js
+++ commands/bonk.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'bonk',
 	description: 'bonk',
 	execute(msg) {
-        return msg.channel.send("My Bot's message", {files: ["https://media1.tenor.com/images/4dee992174206c66cb208bee31174b8d/tenor.gif?itemid=18805247"]});
+        return msg.channel.send(@member, {files: ["https://media1.tenor.com/images/4dee992174206c66cb208bee31174b8d/tenor.gif?itemid=18805247"]});
 	},
 };
