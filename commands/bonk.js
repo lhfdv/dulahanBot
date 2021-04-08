ebe6f470f22a2450ce557dcc9f7ac72d69ee2e09
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-07 22:57:37 -0300

    Update bonk.js

--- commands/bonk.js
+++ commands/bonk.js
@@ -3,12 +3,6 @@ module.exports =
 	name: 'bonk',
 	description: 'bonk',
 	execute(msg) { 
-	const Discord = require('discord.js');
 	const taggedUser = msg.mentions.users.first();
-	const description = `${msg.author} bonks ${taggedUser}`;
-	const bonkEmbed = new Discord.MessageEmbed()
-		.setColor('#0099ff')
-		.setDescription(description)
-		.setImage("https://media1.tenor.com/images/4dee992174206c66cb208bee31174b8d/tenor.gif?itemid=18805247");
-	return msg.channel.send({bonkEmbed});
+	return msg.channel.send({embed: { description: `${msg.author} bonks ${taggedUser}`, image: "https://media1.tenor.com/images/4dee992174206c66cb208bee31174b8d/tenor.gif?itemid=18805247" }});
 }};
