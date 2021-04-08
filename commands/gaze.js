c050042f4a49320a00d3de7312d7baf792b7e121
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-08 09:41:04 -0300

    Update gaze.js

--- commands/gaze.js
+++ commands/gaze.js
@@ -12,25 +12,10 @@
 // 	}});
 // }};
 
-module.exports = 
-	{
+module.exports = {
 	name: 'viera gaze',
 	description: 'viera gaze',
 	execute(msg) { 
 	if(!msg.mentions.users.first()) return msg.channel.send("ERRO: Sem menção para bonkar");
-	const taggedUser = msg.mentions.users.first();
-	const description = `${msg.author} bonks ${taggedUser}`;
-	const rnd_imgs = [
-		'https://i.imgur.com/uqzIUSX.gif',
-		'https://i.imgur.com/Ed6PnCm.gif',
-		'https://i.imgur.com/taqu0OF.gif',
-		'https://i.imgur.com/5pmg59F.gif',
-		'https://i.imgur.com/zhmacyK.gif',
-		'https://i.imgur.com/NwXXKB2.gif'
-	]
-	return msg.channel.send({embed: 
-				 { color: 0xff9900, 
-				   description: description, 
-				   image: { url: rnd_imgs[Math.floor(Math.random() * rnd_imgs.length)] },
-	}});
+	return msg.channel.send('teste');
 }};
