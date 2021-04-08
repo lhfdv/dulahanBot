19806ed8bbd2b8de6ab788e2b63af4c56246d3d2
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-08 14:01:01 -0300

    Update harumi.js

--- commands/harumi.js
+++ commands/harumi.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'harumi',
 	description: 'Harumi',
 	execute(msg) {
-		const list = [ 'um torado' , 'uma aberração', 'um pebado', 'um monstro', 'uma cria de carioca', 'uma cria de jundiaiense' ]
+		const list = [ 'um torado' , 'uma aberração', 'um pebado', 'um monstro', 'uma cria de carioca', 'uma cria de jundiaiense', 'um esculhambado', 'um fajuto' ]
 		return msg.channel.send(`Harumi é ${list[Math.floor(Math.random() * list.length)]}!`);
         }
 };
