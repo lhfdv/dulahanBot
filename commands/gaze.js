a79fe1609f2c800e960c531920188c29611299ee
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-08 09:44:33 -0300

    Update gaze.js

--- commands/gaze.js
+++ commands/gaze.js
@@ -16,5 +16,9 @@ module.exports = {
 	name: 'viera gaze',
 	description: 'viera gaze',
 	execute (msg) { 
-	return msg.channel.send('teste');
+	return msg.channel.send({embed: 
+				 { color: 0x783F04, 
+				   description: `Harumi's Viera gaze at the sheer stupidity of the situation`, 
+				   image: { url: 'https://i.imgur.com/051Tim0.gif' },
+	}});
 }};
