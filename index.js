a64c26f4770a2de8179c8ed79bffa6dd315ebf18
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-08 13:28:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.on('ready', () => {
 })
 
 client.on('message', msg =>{
-	if ( msg.author.bot || msg.content.length > 5 ) return;
+	if ( msg.author.bot || msg.content === 'Harumi' ) return;
 
 	const args = msg.content.trim().split(/ +/);
 	const command = args.shift().toLowerCase();
