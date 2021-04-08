0d544b075597dc35fc5d696926ce8691a294c5c9
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-07 23:03:33 -0300

    Update bonk.js

--- commands/bonk.js
+++ commands/bonk.js
@@ -4,5 +4,9 @@ module.exports =
 	description: 'bonk',
 	execute(msg) { 
 	const taggedUser = msg.mentions.users.first();
-	return msg.channel.send({embed: { color: FFA500, description: `${msg.author} bonks ${taggedUser}`, image: { url: "https://media1.tenor.com/images/4dee992174206c66cb208bee31174b8d/tenor.gif?itemid=18805247" }}});
+	return msg.channel.send({embed: 
+				 { color: 0x0099ff, 
+				 description: `${msg.author} bonks ${taggedUser}`, 
+				image: { url: "https://media1.tenor.com/images/4dee992174206c66cb208bee31174b8d/tenor.gif?itemid=18805247" }
+	}});
 }};
