1c40d5f42969a41b1538091cd0b6b7a90ee4355c
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-07 23:29:47 -0300

    Update bonk.js

--- commands/bonk.js
+++ commands/bonk.js
@@ -6,10 +6,10 @@ module.exports =
 	if(!msg.mentions.users.first()) return msg.channel.send("ERRO: Sem menção para bonkar");
 	const taggedUser = msg.mentions.users.first();
 	const description = `${msg.author} bonks ${taggedUser}`;
+	const img = { url: 'https://i.imgur.com/uqzIUSX.gif' };
 	return msg.channel.send({embed: 
 				 { color: 0xff9900, 
 				   description: description, 
-				   image: { url: 'https://i.imgur.com/uqzIUSX.gif' }
-				  
+				   image: { img },
 	}});
 }};
