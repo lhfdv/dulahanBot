2caaee9308262239d605c10837699c4e30d89f15
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-08 09:31:17 -0300

    Update gaze.js

--- commands/gaze.js
+++ commands/gaze.js
@@ -1,13 +1,36 @@
+// module.exports = 
+// 	{
+// 	name: 'Viera',
+// 	description: 'Viera',
+// 	execute(msg) { 
+// 	const description = `Harumi's Viera gaze at the sheer stupidity of the situation`;
+// 	const img = { url: 'https://i.imgur.com/051Tim0.gif' };
+// 	return msg.channel.send({embed: 
+// 				 { color: 0x783F04, 
+// 				   description: description, 
+// 				   image: img,
+// 	}});
+// }};
+
 module.exports = 
 	{
-	name: 'Viera',
-	description: 'Viera',
+	name: 'viera gaze',
+	description: 'viera gaze',
 	execute(msg) { 
-	const description = `Harumi's Viera gaze at the sheer stupidity of the situation`;
-	const img = { url: 'https://i.imgur.com/051Tim0.gif' };
+	if(!msg.mentions.users.first()) return msg.channel.send("ERRO: Sem menção para bonkar");
+	const taggedUser = msg.mentions.users.first();
+	const description = `${msg.author} bonks ${taggedUser}`;
+	const rnd_imgs = [
+		'https://i.imgur.com/uqzIUSX.gif',
+		'https://i.imgur.com/Ed6PnCm.gif',
+		'https://i.imgur.com/taqu0OF.gif',
+		'https://i.imgur.com/5pmg59F.gif',
+		'https://i.imgur.com/zhmacyK.gif',
+		'https://i.imgur.com/NwXXKB2.gif'
+	]
 	return msg.channel.send({embed: 
-				 { color: 0x783F04, 
+				 { color: 0xff9900, 
 				   description: description, 
-				   image: img,
+				   image: { url: rnd_imgs[Math.floor(Math.random() * rnd_imgs.length)] },
 	}});
 }};
