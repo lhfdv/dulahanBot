b8fe1b9afad322c9b4ddf34fa8452f384c921ec4
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-08 09:17:27 -0300

    Update gaze.js

--- commands/gaze.js
+++ commands/gaze.js
@@ -3,12 +3,10 @@ module.exports =
 	name: 'Viera Gaze',
 	description: 'Viera gazing',
 	execute(msg) { 
-	if(!msg.mentions.users.first()) return msg.channel.send("ERRO: Sem menção para bonkar");
-	const taggedUser = msg.mentions.users.first();
 	const description = `Harumi's Viera gaze at the sheer stupidity of the situation`;
 	const img = { url: 'https://i.imgur.com/051Tim0.gif' };
 	return msg.channel.send({embed: 
-				 { color: 0xff9900, 
+				 { color: 0x783F04, 
 				   description: description, 
 				   image: img,
 	}});
