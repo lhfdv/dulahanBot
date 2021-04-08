c46f3a0e5f3224874e77f38534e53cbb34cfd487
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-07 23:37:32 -0300

    Update bonk.js

--- commands/bonk.js
+++ commands/bonk.js
@@ -7,9 +7,14 @@ module.exports =
 	const taggedUser = msg.mentions.users.first();
 	const description = `${msg.author} bonks ${taggedUser}`;
 	const img = { url: 'https://i.imgur.com/uqzIUSX.gif' };
+	const rnd_imgs = [
+		'https://i.imgur.com/uqzIUSX.gif',
+		'https://i.imgur.com/Ed6PnCm.gif',
+		'https://i.imgur.com/taqu0OF.gif',
+	]
 	return msg.channel.send({embed: 
 				 { color: 0xff9900, 
 				   description: description, 
-				   image: img,
+				   image: { url: rnd_imgs[Math.floor(Math.random() * rnd_imgs.length) },
 	}});
 }};
