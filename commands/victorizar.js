380094f56ceaeaede65f5c106c1bab08a55a81c5
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-08 14:11:13 -0300

    Update victorizar.js

--- commands/victorizar.js
+++ commands/victorizar.js
@@ -2,10 +2,8 @@ module.exports = {
 	name: 'victorizar',
 	description: 'victorizar',
 	execute(msg) {
-		    msg = msg.replace(/(?:s)/g, 'w');
-		    msg = msg.replace(/(?:S)/g, 'W');
-		    msg = msg.replace(/n([aeiou])/g, 'ny$1');
-		    msg = msg.replace(/N([aeiou])|N([AEIOU])/g, 'Ny$1');
-		return msg;
+		    	msg.content = msg.content.replace(/(?:s)/g, 'x');
+		    	msg = msg.content.replace(/(?:S)/g, 'X');
+			return msg;
         }
 };
