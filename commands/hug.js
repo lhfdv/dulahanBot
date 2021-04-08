131321c16059c5b28f6c39c9a93d69f7baed9743
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-08 12:35:48 -0300

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -7,7 +7,7 @@ module.exports = {
 	if(!msg.mentions.users.first()) return msg.channel.send("ERRO: Sem menção para abraçar");
 	const taggedUser = msg.mentions.users.first();
 	let url = `https://api.tenor.com/v1/search?q=anime_hug&key=${process.env.TENORKEY}&limit=50`;
-	let response = await fetch (url);
+	let response = await fetch (url.media.gif);
 	let json = await response.json();
 	let embedcolor = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
 	let index = Math.floor(Math.random() * json.results.length);
