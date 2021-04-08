0bd44af9270929227347f1e4cbe7deff2b2f4299
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-08 02:11:23 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,7 @@
-const discord = require('discord.js')
 module.exports = {
-name: 'help',
-async execute(client, msg, args) {
-  const commandFiles = readdirSync(join(__dirname, "./commands/")).filter(file => file.endsWith(".js")); // Get files
-  const cmdmap = commandFiles.map(files => `${files}`).join(' | Working\n')
-  const embed = new discord.MessageEmbed()
-  .setDescription(cmdmap)
-  return msg.channel.send(embed)
-  }
-}
+	name: 'ixnow',
+	description: 'Ixnow',
+	execute(msg) {
+        return msg.channel.send('Que foi Victória');
+	},
+};
