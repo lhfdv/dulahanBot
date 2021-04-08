8e74d665134f42b574e90a8219f0c0bb920bff9e
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-08 14:28:51 -0300

    Update blush.js

--- commands/blush.js
+++ commands/blush.js
@@ -5,9 +5,9 @@ module.exports = {
     const Discord = require("discord.js");
     let gifs = ['https://media.tenor.com/images/6bdd5b4f5da260636a435d830bccbf25/tenor.gif',
     'https://media1.tenor.com/images/ea7294f9919cd75b9a5217b818a082e3/tenor.gif?itemid=15363712' ]
-    const embed = new RichEmbed()
+    const embed = new Discord.MessageEmbed()
         .setColor('#'+(Math.random()*0xFFFFFF<<0).toString(16))
-        .setAuthor(`${msg.author.username} blushes (⁄ʘ⁄ ⁄ ω⁄ ⁄ ʘ⁄)♡`, message.author.avatarURL)
+        .setAuthor(`${msg.author.username} blushes (⁄ʘ⁄ ⁄ ω⁄ ⁄ ʘ⁄)♡`, msg.author.avatarURL)
         .setImage(gifs[Math.floor(Math.random() * gifs.length)]);
     return msg.channel.send(embed);
     }
