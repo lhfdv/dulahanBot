5a0d5ce9f03d722fc5b63fccf4dc8c55251ac988
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-08 10:56:29 -0300

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -8,10 +8,13 @@ module.exports = {
 	let url = `https://api.tenor.com/v1/search?q=anime_hug&key=${process.env.TENORKEY}&limit=10`;
 	let response = await fetch (url);
 	let json = await response.json();
+	let embedcolor = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
 	let index = Math.floor(Math.random() * json.results.length);
 	const urlImg = json.results[index].url;
 	const description = `${msg.author} hugs ${taggedUser} ${urlImg}`;
-	return msg.channel.send({embed:
-				 { color: 0xff9900, 
-				   description: description}})
+	const embed = new Discord.RichEmbed()
+            .setDescription(description)
+            .setColor(embedcolor)
+            .setImage(`${imgUrl}`)
+        return message.channel.send({embed: embed});	
 	}};
