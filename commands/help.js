2bf7f032e166d1b9c82addf49ccd422df4bb1148
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-08 02:15:35 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 module.exports = {
-	name: 'ixnow',
-	description: 'Ixnow',
+	name: 'help',
+	description: 'help',
 	execute(msg) {
-        return msg.channel.send('Que foi Victória');
+        return msg.channel.send('Comandos: Ixnow | Help | Fatos Harumisticos | João Pedro | Bonk <mention> | Harumi | Ping | Mecânica Mata Jo');
 	},
 };
