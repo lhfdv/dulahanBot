9dcb65d8b04e217e16e5e1996cbc3dddadf341ab
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-09 16:54:43 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,9 +2,9 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(msg, args) {
-		const userFernando = 418776618786619392
-		if(msg.sender === userID) { msg.channel.send('Sossega') }
+		const userID = 418776618786619392
+		if(msg.author.id === userID) { msg.channel.send('Sossega') }
 
-		if(!msg.sender === userID) { msg.channel.send('Pong') }
+		if(!msg.author.id === userID) { msg.channel.send('Pong') }
 	},
 }; 
