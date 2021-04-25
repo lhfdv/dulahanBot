3517c988245500f4a98d0e8ac22142fa256105c0
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-25 04:13:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
-const config = require('.config.json')
+const config = require('./config.json')
 const mongo = require('./utils/mongo.js')
 require('dotenv').config();
 
