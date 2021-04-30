25db0ba6b6e8e0dc67ef5615a3254350eb136374
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-04-30 20:50:01 -0300

    Update gaze.js

--- cmd/gaze.js
+++ cmd/gaze.js
@@ -5,6 +5,6 @@ module.exports = {
 	msg.channel.send({embed: 
 		{ color: 0x783F04, 
 		  description: `Harumi's Viera stares at the sheer stupidity of the situation`, 
-		  image: { url: 'https://i.imgur.com/051Tim0.gif' },
+		  image: { url: 'https://i.imgur.com/HN0ubk5.gif' },
 	}});
 }};
