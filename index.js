10393d334f0b82cab1857463a67be025ae768c20
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-05-14 01:43:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,35 @@ client.on('ready', () => {
 	client.user.setActivity('theHunter: Call of the Wild')
 	
 	client.api.applications(client.user.id).guilds('733378438903365662').commands.post({
-		data: { client.commands.set(command.name, command) }
+		data: { 
+			name: "data",
+			description: "test"
+		}
 	})
+	
+	client.ws.on('INTERACTION_CREATE', async interaction => {
+		const command = interaction.data.name.toLowerCase();	
+		const args = interaction.data.options;
+		
+		if(command === 'hello'){
+			client.api.interactions(interaction.id, interaction.token).callback.post({
+				data: {
+            type: 4,
+            data: {
+            content: "hello"
+            }
+					}
+				})
+      }
+
+      client.api.interactions(interaction.id, interaction.token).callback.post({
+        data: {
+          type: 4,
+          data: await createAPIMessage(interaction, embed)
+        }
+      })
+
+    })
 	
 //     	await mongo().then(mongoose => {
 //         try {
@@ -41,7 +68,12 @@ client.on('ready', () => {
 //             mongoose.connection.close()
 //         }
 //    })
-})
+
+async function createAPIMessage(interaction, content) {
+	const apiMessage = await discord.APIMessage.create(client.channels.resolve(interaction.channel_id), content)
+	.resolveData(0)
+	.resolveFiles();
+}
 
 // client.on('ready', async () => {
 //     messageCount(client);
