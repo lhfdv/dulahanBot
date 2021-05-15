5e28737af447453e12f7a18cde2bf3c8158e3117
Lucas Franco <lhfranco@protonmail.com>
2021-05-15 06:10:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const Discord = require('Discord.js')
+const Discord = require('discord.js')
 const client = new Discord.Client()
 const fs = require('fs')
 const commandData = require('./commands.js')
