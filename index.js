668128e84002ab09fcf91fac95fff3af579e1f56
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-10-25 08:53:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ client.on('message', message => {
       switch (args[0]){
             case 'play':
                 function play(connection, msg){
-                    var server = servers[msg.guild.id];
+                    var server = servers[message.guild.id];
                     server.dispatcher = connection.play(ytdl(server.queue[0], {filter: "audioonly"}));
                     server.queue.shift();
                     server.dispatcher.on("end", function(){
@@ -101,44 +101,44 @@ client.on('message', message => {
                         }
                     })
                 }
-                if(!msg.member.voice.channel){
-                    msg.channel.send("Entra num canal");
+                if(!message.member.voice.channel){
+                    message.channel.send("Entra num canal");
                     return;
                 }
-                if(!servers[msg.guild.id]) servers[msg.guild.id] = {
+                if(!servers[message.guild.id]) servers[message.guild.id] = {
                     queue: []
                 }
                 var server = servers[msg.guild.id];
 
                 server.queue.push(args[1]);
 
-                if(!msg.guild.voiceConnection) msg.member.voice.channel.join().then(function(connection){
-                    msg.channel.send("Tocando música pa nóis");
-                    play(connection, msg);
+                if(!message.guild.voiceConnection) message.member.voice.channel.join().then(function(connection){
+                    message.channel.send("Tocando música pa nóis");
+                    play(connection, message);
                 })
                 if(!args[1]){
-                    msg.channel.send("Cade o link pro pai?");
+                    message.channel.send("Cade o link pro pai?");
                     return;
                 }
             break;
 
             case 'skip':
-                var server = servers[msg.guild.id];
+                var server = servers[message.guild.id];
                 if(server.dispatcher) server.dispatcher.end();
-                msg.channel.send("Pulinho da Vieira");
+                message.channel.send("Pulinho da Vieira");
             break;
 
             case 'stop':
-                var server = servers[msg.guild.id];
-                if(msg.guild.voice.connection){
+                var server = servers[message.guild.id];
+                if(message.guild.voice.connection){
                     for(var i = server.queue.length-1; i >= 0; i--){
                         server.queue.splice(i, 2);
                     }
                     server.dispatcher.end();
-                    msg.channel.send("Chega de música, tá chovendo aqui");
+                    message.channel.send("Chega de música, tá chovendo aqui");
                     //console.log('To parando');
                 }
-                if(msg.guild.connection) msg.guild.voice.connection.disconnect();
+                if(message.guild.connection) message.guild.voice.connection.disconnect();
             	break;
             }
 	
