ea49d1a23ec5d0340eedcdad267fb9233b67eeb6
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-10-26 18:39:15 -0300

    Update bleach.js

--- modules/js/bleach.js
+++ modules/js/bleach.js
@@ -2,28 +2,18 @@ module.exports = {
 	name: 'bleach',
 	description: 'bleach',
 	execute (message) {
+		const selectE = ['e', '𝖾', '𝚎' ]
+		const selectA = ['a', '𝖺', '𝚊']
+		const selectL = ['l', '𝚕', '𝗅']
+		const randomE = selectE[Math.floor(Math.random() * selectE.length)]
+		const randomL = selectL[Math.floor(Math.random() * selectL.length)]
+		const randomA = selectA[Math.floor(Math.random() * selectA.length)]
 		const phrases = [
-			'Ble𝚊ch é muito ruim',
-      			'Ble𝚊ch sempre foi ruim',
-			'Bleach é MT RUIM',
-			'B𝚕e𝚊ch é ruim',
-			'Mas você sabe que Bleach é ruim, pô',
-			'Bl𝖾ach é muito ruim',
-			'Bl𝖾ach sempre foi ruim',
-			'Bl𝚎ach é MT RUIM',
-			'Bl𝚎ach é ruim',
-			'Mas você sabe que Bl𝖾ach é ruim, pô',
-			'B𝚕e𝚊ch é ruim',
-			'Mas você sabe que Bleach é ruim, pô',
-			'B𝚕𝖾ach é muito ruim',
-			'B𝚕𝖾ach sempre foi ruim',
-			'B𝚕𝚎ach é MT RUIM',
-			'B𝚕𝚎ach é ruim',
-			'Mas você sabe que B𝚕𝖾ach é ruim, pô',
-			'B𝚕𝖾ac𝚑 sempre foi ruim',
-			'B𝚕𝚎ac𝚑 é MT RUIM',
-			'B𝚕𝚎ac𝚑 é ruim',
-			'Mas você sabe que B𝚕𝖾ac𝚑 é ruim, pô',
+			'B' + randomL + randomE + randomA + 'ch é muito ruim',
+      			'B' + randomL + randomE + randomA + 'ch sempre foi ruim',
+			'B' + randomL + randomE + randomA + 'ch é MT RUIM',
+			'B' + randomL + randomE + randomA + 'ch é ruim',
+			'Mas você sabe que B' + randomL + randomE + randomA + 'ch é ruim, pô',
 		]
 		const response = phrases[Math.floor(Math.random() * phrases.length)];
 		message.channel.send(response);
