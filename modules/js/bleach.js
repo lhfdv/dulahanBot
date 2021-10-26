da9d4b85c3313b463ccfe82f6318812b6b3d1706
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-10-26 18:44:30 -0300

    Update bleach.js

--- modules/js/bleach.js
+++ modules/js/bleach.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'bleach',
 	description: 'bleach',
 	execute (message) {
-		const selectE = ['e', '𝖾', '𝚎' ]
+		const selectE = ['e', '𝖾', '𝚎']
 		const selectA = ['a', '𝖺', '𝚊']
 		const selectL = ['l', '𝚕', '𝗅']
 		const randomE = selectE[Math.floor(Math.random() * selectE.length)]
@@ -11,9 +11,9 @@ module.exports = {
 		const phrases = [
 			'B' + randomL + randomE + randomA + 'ch é muito ruim',
       			'B' + randomL + randomE + randomA + 'ch sempre foi ruim',
-			'B' + randomL + randomE + randomA + 'ch é MT RUIM',
-			'B' + randomL + randomE + randomA + 'ch é ruim',
-			'Mas você sabe que B' + randomL + randomE + randomA + 'ch é ruim, pô',
+			'B' + randomL + randomE + randomA + 'ch é MT RUIM',
+			'B' + randomL + randomE + randomA + 'ch é ruim',
+			'Mas você sabe que B' + randomL + randomE + randomA + 'ch é ruim, pô',
 		]
 		const response = phrases[Math.floor(Math.random() * phrases.length)];
 		message.channel.send(response);
