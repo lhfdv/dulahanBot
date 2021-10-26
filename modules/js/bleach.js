a8194fafaedf7de9fbd3f287d1134f545bbf43e5
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-10-26 09:31:13 -0300

    Update bleach.js

--- modules/js/bleach.js
+++ modules/js/bleach.js
@@ -7,7 +7,12 @@ module.exports = {
       'Bleach sempre foi ruim',
 			'Bleach é MT RUIM',
 			'Bleach é ruim',
-			'Mas você sabe que Bleach é ruim, pô'
+			'Mas você sabe que Bleach é ruim, pô',
+			'Bl𝖾ach é muito ruim',
+			'Bl𝖾ach sempre foi ruim',
+			'Bl𝖾ach é MT RUIM',
+			'Bl𝖾ach é ruim',
+			'Mas você sabe que Bl𝖾ach é ruim, pô',
 		]
 		const response = phrases[Math.floor(Math.random() * phrases.length)];
 		message.channel.send(response);
