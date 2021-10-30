ba39b726c02d4a75acfd97d2d8c83f6a2ed3d521
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-10-30 02:35:54 -0300

    Delete joao.js

--- modules/js/joao.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'joão pedro',
-	description: 'João Pedro',
-	execute(msg) {
-		const list = [ 'Mapa' , 'Bom, bom pra quem?', 'Ha ha ha ha ha', 'Clã, viramo notícia' ]
-		msg.channel.send(`${list[Math.floor(Math.random() * list.length)]}`);
-	},
-};
