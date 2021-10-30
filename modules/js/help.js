67e7b569a554d77a3de53c93e98f3c9a4952b811
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-10-30 15:39:44 -0300

    Update help.js

--- modules/js/help.js
+++ modules/js/help.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'help',
 	description: 'help',
 	execute(msg) {
-        msg.channel.send('Comandos: Ixnow | Help | Fatos Harumisticos | Bonk <mention> | Hug <mention> | Harumi | Viera Gaze | Ping ');
+        msg.channel.send('Comandos: Ixnow | Help | Fatos Harumisticos | Bonk <mention> | Hug <mention> | Harumi | Viera Gaze | Ping | Fabrizio ');
 	},
 };
