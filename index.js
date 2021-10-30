7907e57fd19ec54f06a68c354c7480102a03ea2b
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-10-30 02:59:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const fs = require('fs')
-const commandData = require('./commands.js')
 const ytdl = require("ytdl-core");
 require('dotenv').config()
 
