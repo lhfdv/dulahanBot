07590c5a2686c43ff9c27d502a2921c29de72d4d
Lucas Franco <lhfranco@protonmail.com>
2022-04-28 15:28:06 -0300

    lol tests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const embed = new Discord.MessageEmbed
 const fs = require('fs')
 const ytdl = require("ytdl-core");
 require('dotenv').config()
@@ -84,6 +85,31 @@ client.on('message', message => {
         return
     }
 	
+});
+
+client.on('presenceUpdate', (newMember) => {
+    member = newMember;
+    if (newMember.user.bot) return;
+    
+    activityLength = newMember.member.presence.activities.length;
+
+    //check to see if the user has an activities, and if so, how many
+    if (activityLength >0 ){
+        console.log("member has " + activityLength + " activities");
+
+        for (let i = 0; i < activityLength; i++) {         
+          
+        //Debugging messages to the log
+        console.log("Activity in position " + i + " is " + newMember.member.presence.activities[i].name.toLowerCase());
+        //console.log("now in lower case " + newMember.member.presence.activities[0].name.toLowerCase());
+        //If you want to ban players of any other game than LOL, changer where it says league of legends to any other lowercase name of a game
+        if (newMember.member.presence.activities[i].name.toLowerCase() == "league of legends") { // Started playing.
+            message.channel.send(`${newMember.user.id} https://www.youtube.com/watch?v=9Deg7VrpHbM`);
+        }
+    }
+    } else {
+        console.log("member has no activities");
+    }
 });
 
 client.login(process.env.token)
