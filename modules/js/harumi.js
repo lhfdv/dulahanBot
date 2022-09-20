478f69dbc5f7bbe57a63f75fae90a41e23747191
lhfdv <47636364+lhfdv@users.noreply.github.com>
2022-09-20 10:03:28 -0300

    Delete harumi.js

--- modules/js/harumi.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'harumi',
-	description: 'Harumi',
-	execute (msg) {
-		const list = [ 'um torado' , 'uma aberração', 'um pebado', 'um monstro', 'uma cria de carioca', 'uma cria de jundiaiense', 'um esculhambado', 'um fajuto' ]
-		msg.channel.send(`Harumi é ${list[Math.floor(Math.random() * list.length)]}!`);
-    }
-};
